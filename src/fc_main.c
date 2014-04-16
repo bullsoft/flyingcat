b278960f17326af4a65b9392f19942b34fe8b5d3
ideal <idealities@gmail.com>
2014-04-16 12:23:04 +0800

    it's wednesday

--- src/fc_main.c
+++ src/fc_main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 BullSoft
+ * Copyright (C) 2014 Shang Yuanchun <idealities@gmail.com>
  *
  *
  * You may redistribute it and/or modify it under the terms of the
@@ -7,13 +8,13 @@
  * Foundation; either version 3 of the License, or (at your option)
  * any later version.
  *
- * FishPlayer is distributed in the hope that it will be useful,
+ * FlyingCat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with FishPlayer. If not, write to:
+ * along with FlyingCat. If not, write to:
  * The Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor
  * Boston, MA 02110-1301, USA.
