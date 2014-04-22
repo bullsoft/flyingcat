16fd14b6099aab304d66ccca0280962a3edb2e07
ideal <idealities@gmail.com>
2014-04-22 14:58:33 +0800

    change string macro

--- src/fc_string.h
+++ src/fc_string.h
@@ -25,11 +25,11 @@
 
 #include <stdarg.h>
 
-#define fc_scnprintf(buf, size, ...)          \
-        _scnprintf((char *)(buf), (size_t)(size), __VA_ARGS__)
+#define fc_scnprintf(_buf, _size, ...)          \
+        _scnprintf((char *)(_buf), (size_t)(_size), __VA_ARGS__)
     
-#define fc_vscnprintf(buf, size, fmt, args)   \
-        _vscnprintf((char *)(buf), (size_t)(size), fmt, args)
+#define fc_vscnprintf(_buf, _size, _fmt, _args)   \
+        _vscnprintf((char *)(_buf), (size_t)(_size), _fmt, _args)
 
 int _scnprintf(char *buf, size_t size, const char *fmt, ...);
 int _vscnprintf(char *buf, size_t size, const char *fmt, va_list args);
